79d6391c7a1565ccb1b8a4e413b044404919b172
ArunKumar0707 <115449190+ArunKumar0707@users.noreply.github.com>
2023-11-23 19:43:52 +0530

    Delete backend directory

--- backend/config.js
+++
@@ -1,7 +0,0 @@
-export const PORT = 5555;
-
-export const MongoDBURL=
-'mongodb+srv://root:root@books-store-mern.e2mwygf.mongodb.net/books-collection?retryWrites=true&w=majority';
-
-//Please create a free databse for yourself
-//This database will be deleted after
